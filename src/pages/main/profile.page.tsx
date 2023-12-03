77e15e772003c2dcfe2df732f960b8f22c97604d
Nhat Bui <nhaths4701@gmail.com>
2023-12-03 22:46:31 +0700

    feat: add lib react-qr-scanner

--- src/pages/main/profile.page.tsx
+++ src/pages/main/profile.page.tsx
@@ -1,5 +1,15 @@
+import { QrScanner } from '@yudiel/react-qr-scanner';
+
 const ProfilePage = () => {
-  return <h1>Profile page</h1>;
+  return (
+    <>
+      <QrScanner
+        onDecode={(result) => console.log(result)}
+        onError={(error) => console.log(error?.message)}
+      />
+      <h1>Profile page</h1>
+    </>
+  );
 };
 
 export default ProfilePage;
